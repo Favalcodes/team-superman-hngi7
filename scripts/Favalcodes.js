4354b9071d54b8b44dfa9e868a5db16baab1faea
Favour Godwin Arua <52804141+Favalcodes@users.noreply.github.com>
2020-06-03 09:30:54 +0100

    Create Favalcodes.js

---
+++ scripts/Favalcodes.js
@@ -0,0 +1,7 @@
+const fullName = "Favour Ezinne Arua";
+const id = "HNG-04184";
+let lang = "JavaScript";
+
+console.log(
+"Hello World, this is " + fullName + " with HNGi7 ID " + id + " using " + lang + " for stage 2 task."
+);
